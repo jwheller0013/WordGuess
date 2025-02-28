16fb5cd25b3babd4f41b9be88d60c86684658441
jwheller0013 <jwheller0013@yahoo.com>
2025-02-28 16:53:12 -0500

    save point

--- src/main/java/com/github/zipcodewilmington/Hangman.java
+++ src/main/java/com/github/zipcodewilmington/Hangman.java
@@ -10,12 +10,20 @@ public class Hangman {
 
     String[] words = {"car", "cat", "code", "bar", "bed", "dead", "home"};
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        Wordguess game = new Wordguess();
+        game.runGame();
+    }
 
-}
 public static String randomword(){
         int num = (int) (Math.random() * words.length);
         return words[num];
 }
 
-}
+// while number of guesses is less than word length keep playing while
+// Game start once word picked print "_" equal to word length
+// as char scanned equal char(#) replace in ____
+// as char wrong +1 on guesses
+// report a win statement if correct guesses equal word length
+// report loss if guesses = word length
+// loss and win end in request play again
---
+++ src/main/java/com/github/zipcodewilmington/Wordguess.java
@@ -0,0 +1,7 @@
+package com.github.zipcodewilmington;
+
+public class Wordguess {
+    public  void runGame(){
+        System.out.println("Game On");
+    }
+}
