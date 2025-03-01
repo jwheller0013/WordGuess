bcfddf6fe20e718e5d05d9ca519b23c040342b01
jwheller0013 <jwheller0013@yahoo.com>
2025-03-01 16:49:06 -0500

    Stuck, come back later

--- src/main/java/com/github/zipcodewilmington/Hangman.java
+++ src/main/java/com/github/zipcodewilmington/Hangman.java
@@ -1,23 +1,46 @@
 package com.github.zipcodewilmington;
 // James Heller
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.Arrays;
 
 /**
  * @author xt0fer
  * @version 1.0.0
  * @date 5/27/21 11:02 AM
  */
-public class Hangman {
 
-    String[] words = {"car", "cat", "code", "bar", "bed", "dead", "home"};
+public class Hangman {
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.Arrays;
 
     public static void main(String[] args){
-        Wordguess game = new Wordguess();
-        game.runGame();
-    }
+        String[] words = {"car", "cat", "code", "bar", "bed", "dead", "home"};
 
-public static String randomword(){
-        int num = (int) (Math.random() * words.length);
-        return words[num];
+//        Wordguess game = new Wordguess();
+//        game.runGame();
+//    }
+
+        String chosenWord[] = words[(Math.random() * words.length)]; //chosenWord should become one of the words
+        String[] dashes = new String[chosenWord.length];
+        Arrays.fill(dashes, "_"); //makes _ for chosenWord length
+        char [] wordArray = chosenWord.toCharArray ();
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a letter: ");
+        input = scanner.next(); //Scan letter guessed
+
+        int guesses = 0;
+
+        while(Arrays.toString(dashes).contains("_")) {
+
+
+            guesses
+        }
+
+
+
 }
 
 // while number of guesses is less than word length keep playing while
